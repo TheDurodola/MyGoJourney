19e5487eca56c3df95fac0e79b4395fce3767eac
Durodola Abolaji <bolajidurodola@gmail.com>
2026-02-24 14:46:41 +0100

    porgress

---
+++ src/golang-book/chapter-9/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+
+func main(){
+	var circle Circle
+	circle.diameter = 8
+
+	fmt.Println(circle.diameter)
+
+	triangle := new(Triangle)
+	triangle.x = 8
+
+	circle1 := Circle{diameter: 7, radius: 4}
+
+	fmt.Printf("circle1.diameter: %v\n", circle1.diameter)
+
+
+	fmt.Printf("circleArea(circle): %v\n", circleArea(circle1))
+
+
+	circle.area()
+
+}
+
+
+type Circle struct{
+	diameter int8
+	radius int8
+
+}
+
+type Triangle struct{
+	x, y, z float64
+}
+
+
+func circleArea(c Circle) float64 {
+	return math.Pi * float64(c.radius) * float64(c.radius)
+}
+
+//
+func (c Circle) area() float64 {
+	return math.Acos(float64(c.diameter))
+}
+
+
+// is-a relationship
+type Polygon struct{
+	Circle
+	Triangle
+}
+
+//  has-a relationship
+type Omo struct {
+	circle Circle
+	triangle Triangle
+}
